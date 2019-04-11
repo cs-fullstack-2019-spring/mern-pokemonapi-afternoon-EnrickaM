508ad94fee474e324c5499567fc9351d53d32f82
EnrickaM <marshallenricka@yahoo.com>
2019-04-11 16:05:21 -0500

    classwork

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            data:[]
+        };
+    }
+    componentDidMount() {
+        this.loadData();
+    }
+    loadData(){
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=900")
+            .then(data=>data.json())
+            .then(jsonedData=>this.setState({data:jsonedData.results}))
+    }
+
+
+    render() {
+        let DataMap=this.state.data.map(
+            (eachElement,index)=>{
+
+                return(<div>
+                        <h2>{(index)+ eachElement.name+""}</h2>
+                        <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+index+".png"} alt=""></img>
+                    </div>
+
+                )
+            }
+        )
+    return (
+      <div className="App">
+        <h1>Pokemon</h1>
+          {DataMap}
+      </div>
+    );
+  }
+}
+
+export default App;
